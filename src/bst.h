18ec9d5547aac7a92f225f0f9043d55035a9e6e0
Karim <karamanov2007@gmail.com>
2026-02-21 14:38:38 +0300

    add func from taskA
    Co-authored-by: EStarikov <egor.staikov2007@gmail.com>

---
+++ src/bst.h
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+typedef struct Node {
+    int value;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+typedef struct {
+    Node* root;
+    int size;
+} BST;
+
+int bstInsert(BST* tree, int value);
+BST* InitTree();
+void bstFree(BST* tree);
+bool bstContains(BST* tree, int value);
