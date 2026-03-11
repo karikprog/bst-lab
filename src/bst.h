a033fd9f356b1410ac18cf913d359007c7f1a3ef
Karim <karamanov2007@gmail.com>
2026-03-12 01:21:49 +0300

    add tree valid func

--- src/bst.h
+++ src/bst.h
@@ -18,7 +18,11 @@ BST* initTree();
 Node* search(Node* node, int value);
 void bstFree(BST** treeRef);
 bool bstContains(BST* tree, int value);
+<<<<<<< HEAD
 bool bstMax(const BST* tree, int* result);
 bool bstMin(const BST* tree, int* result);
 int bstSize(const BST* tree);
 int bstHeight(const BST* tree);
+=======
+bool bstIsValid(const BST* tree);
+>>>>>>> 6640205 (add tree valid func)
